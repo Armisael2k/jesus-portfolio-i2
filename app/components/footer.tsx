a168e0afd4fc3c76d222f3d5a2d85f0413a1e0e7
Armisael2k <chuylopez755@gmail.com>
2026-03-12 12:49:21 -0700

    added footer git link

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,10 +1,15 @@
 import url from "@/data/web-url"
+import { FaGithub } from "react-icons/fa";
+import { Link } from "@components/ui/link";
 
 export default function Footer() {
   return (
     <div className="pt-5 pb-10 relative px-6">
-      <div className="container mx-auto flex justify-between">
+      <div className="container mx-auto flex justify-center items-center gap-2">
         <p className="text-sm text-ctp-subtext0">© {new Date().getFullYear()} {url}</p>
+        <Link to="https://github.com/Armisael2k/jesus-portfolio-i2" target="_blank">
+          <FaGithub />
+        </Link>
       </div>
     </div>
   )
